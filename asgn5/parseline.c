9e96882d593b523a817055b604bddc9298707ae6
Caitlin Settles <csettles@calpoly.edu>
2018-03-03 14:42:41 -0800

    Fixing off by one error for initial line input

--- asgn5/parseline.c
+++ asgn5/parseline.c
@@ -9,38 +9,46 @@
 #include "parseline.h"
 
 int main(int argc, const char * argv[]) {
-	char *stages[STAGE_MAX], line[LINE_MAX + 1];
-	int i, len = 0;
-	
+	char *stages[STAGE_MAX], line[LINE_MAX + 2];
+	int i, len = 0, str_len;
+    
 	printf("line: ");
-	fgets(line, LINE_MAX + 1, stdin);
-
-	len = split_line(line, stages);
+	fgets(line, LINE_MAX + 2, stdin);
+	
+        str_len = (int)strlen(line);
+        if (str_len > LINE_MAX) {
+                fprintf(stderr, "parseline: command line length too long\n");
+		exit(EXIT_FAILURE);
+        } else {
+                printf("%d\n", str_len);
+        }
 	
 	for (i = 0; i < len; i++) {
 		/* process stage */;
 	}
 	
-	return 0;
+	len = split_line(line, stages);
+
+    return 0;
 }
 
 int split_line(char *line, char **stages) {
-	char *token;
-	int len = 1; /* any input is automatically a stage */
-	
-	token = strtok(line, "|");
-	
-	while (token != NULL && len < STAGE_MAX) {
-		*stages = token;
-		token = strtok(NULL, "|");
-		stages++, len++;
-	}
-	*stages = NULL;
-	
-	if (token != NULL) {
-		/* token would be NULL if */
-		fprintf(stderr, "pipeline too deep\n");
-		exit(EXIT_FAILURE);
-	}
-	return len;
+    char *token;
+    int len = 1; /* any input is automatically a stage */
+    
+    token = strtok(line, "|");
+    
+    while (token != NULL && len < STAGE_MAX) {
+        *stages = token;
+        token = strtok(NULL, "|");
+        stages++, len++;
+    }
+    *stages = NULL;
+    
+    if (token != NULL) {
+        /* token would be NULL if */
+        fprintf(stderr, "pipeline too deep\n");
+        exit(EXIT_FAILURE);
+    }
+    return len;
 }
