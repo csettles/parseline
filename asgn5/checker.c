01d0b79161fcc48e95fe598525d0280bb00e5185
Caitlin Settles <csettles@calpoly.edu>
2018-03-03 19:31:46 -0800

    Add stage number to handle functions

--- asgn5/checker.c
+++ asgn5/checker.c
@@ -3,25 +3,32 @@
 /* Will take in the array of arguments and do stuff with it */
 
 /** Will take in an input string **/
-int handle_line(char *input) {
-        handle_input(input);
-        handle_output(input);
-        handle_args(input);
+int handle_line(char *input, int stageno) {
+	char *rest;
+	rest = strtok(input, " ");
+	/* input is now the command */
+	
+        handle_input(rest, stageno);
+        handle_output(rest, stageno);
+        handle_args(rest, stageno);
         return 0;
 }
 
 /** Will determine where the input is coming from **/
-int handle_input(char *input) {
+int handle_input(char *input, int stageno) {
+	/* find first < */
+	/* if there is no redirection, input is stdin for stage #0
+	 and the previous stage for stages 1+ */
         return 0;
 }
 
 /** Will determine where the output should be **/
-int handle_output(char *input) {
+int handle_output(char *input, int stageno) {
         return 0;
 }
 
 /** Will get the arg list and size**/
-int handle_args(char **arguments, char *input) {
+int handle_args(char **arguments, char *input, int stageno) {
         return 0;
 }
 
--- asgn5/checker.h
+++ asgn5/checker.h
@@ -1,9 +1,9 @@
 #ifndef CHECKER_H
 #define CHECKER_H
 
-int handle_line(char *input);
-int handle_input(char *input);
-int handle_output(char *input);
-int handle_args(char *input);
+int handle_line(char *input, int stagno);
+int handle_input(char *input, int stageno);
+int handle_output(char *input, int stageno);
+int handle_args(char *input, int stageno);
 
 #endif
